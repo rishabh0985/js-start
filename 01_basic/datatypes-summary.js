7abd376c7906ec5c63f34df409075a0e62e14f60
rishabh0985 <128125196+rishabh0985@users.noreply.github.com>
2023-09-26 07:49:35 +0000

    data-summery

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -35,3 +35,31 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (non-primitive)
+
+let myName = "google"
+
+let anotherName = myName 
+anotherName = "youtube"
+console.log(myName);
+console.log(anotherName);    
+
+// joh name change ho sakthe hain woh primitive mtplb (Stack) khethe hain 
+
+let userOne = {
+    email: "google@com",
+    upi: "abc@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "youtube@com"
+
+console.log(userOne);
+console.log(userTwo);
+
+// joh name change ho sakthe hain unhe Non-Primitive mtlb (Heap) kehthe hain 
